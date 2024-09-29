d9df08e3d71c21e89db7e154f0357fddfd1f3700
Hideo Takahashi <takahashi-hideo543@g.ecc.u-tokyo.ac.jp>
2024-09-29 16:08:48 +0900

    version for 24/09/29

--- scripts/run_time_evo_h1.sh
+++ scripts/run_time_evo_h1.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -e
 
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <device> <cuStateVec_enable> <dim> <precision> <bits>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 <device> <cuStateVec_enable> <dim> <precision> <bits> <useSavedFile>"
     exit 1
 fi
-
-DEVICE=$1
-CUSTATEVEC=$2
-DIM=$3
-PRECISION=$4
-BITS=$5
 
 source ../../.venv/bin/activate
 source ../../.env
 export PYTHONPATH
 
-python3 ./time_evo_h1.py --device $1 --cuStateVec_enable $2 --dim $3 --precision $4 --bits $5
+python3 ./time_evo_h1.py --device $1 --enable-cuStateVec $2 --dim $3 --precision $4 --bits $5 --use-saved-data $6
--- scripts/time_evo_h1.py
+++ scripts/time_evo_h1.py
@@ -1,6 +1,7 @@
 import math, os, argparse
 import numpy as np
 import scipy.special as sp
+from matplotlib import pyplot as plt
 
 from crsq.blocks.antisymmetrization import AntisymmetrizationSpec
 from crsq.blocks.discretization import DiscretizationSpec
@@ -15,6 +16,8 @@ from qiskit import transpile
 import crsq.utils.statevector as svec
 
 import logging
+
+logger = logging.getLogger('TEV')
 
 # make one dimentional wave function data for H atom.
 
@@ -34,9 +37,9 @@ def hydrogen1d_psi(xa: np.ndarray,x0: float,N: int):
 # build the simulator
 
 class Parameters:
-    def __init__(self, device='GPU', cuStateVec_enable=True, dim=1, precision='single', n1=5):
+    def __init__(self, device='GPU', enable_cuStateVec=True, dim=1, precision='single', n1=5, use_saved_data=False):
         self.device = device
-        self.cuStateVec_enable = cuStateVec_enable
+        self.enable_cuStateVec = enable_cuStateVec
         self.precision = precision
         self.dim = dim  # 1 dimension
         self.n1 = n1 # bits per coordinate
@@ -55,10 +58,10 @@ class Parameters:
         self.nuclei_data = [{"mass": 1680, "charge": 1, "pos": (self.M//2, self.M//2)}]
 
         self.ham_spec = HamiltonianSpec(self.wfr_spec, nuclei_data=self.nuclei_data)
+        self.use_saved_data = use_saved_data
+        self.stm_block = None
     
     def draw_circuits(self, outdir):
-        logger = logging.getLogger('TEV')
-
 
         self.x = np.linspace(0, self.L-self.dq, self.M)
         self.x0 = 8
@@ -79,39 +82,34 @@ class Parameters:
         self.num_elec_it = 10 # number of electron iterations
         self.evo_spec = TimeEvolutionSpec(self.ham_spec, self.disc_spec, self.num_nucl_it, self.num_elec_it)
 
-        stm = SuzukiTrotterMethodBlock(self.evo_spec, self.ene_spec, self.asy_spec)
+        self.stm_block = SuzukiTrotterMethodBlock(self.evo_spec, self.ene_spec, self.asy_spec)
 
         logger.info('draw the circuit')
 
         fname= outdir + "/h1d.circuit.png"
-        stm.circuit.draw(output='mpl', filename=fname, scale=0.6)
+        self.stm_block.circuit.draw(output='mpl', filename=fname, scale=0.6)
 
         # draw the circuit
 
-        epot = stm.build_elec_potential_block()
+        epot = self.stm_block.build_elec_potential_block()
         fname= outdir + "/h1d.circuit.elec_potential.png"
         epot.circuit.draw(output='mpl', filename=fname, scale=0.6)
 
 
+    def _make_filename(self, outdir, time):
+        dirname = outdir
+        basename = "h1d"
+        fname = f"{dirname}/{basename}.{self.n1}b.mu{self.x0}t{time}.csv"
+        return fname
+
     def run_circuit(self, outdir):
         # run the simulator
-        logger = logging.getLogger('TEV')
-
         logger.info('run the simulator')
-
-        run_calculation=True
-        # run_calculation=False
-
-        dirname= outdir
-
-        basename="h1d"
-
         #num_iters = [1, 10, 100, 500, 1000, 1500]
         num_iters = [1]
         # num_iters = [10]
 
-        # backend = AerSimulator(method="statevector", device="GPU", cuStateVec_enable=False, precision="single")
-        backend = AerSimulator(method="statevector", device=self.device, cuStateVec_enable=self.cuStateVec_enable, precision=self.precision)
+        backend = AerSimulator(method="statevector", device=self.device, cuStateVec_enable=self.enable_cuStateVec, precision=self.precision)
         backend.set_options(max_parallel_threads=0)
         for num_elec_it in num_iters:
             num_nucl_it = 1 # number of nucleus iterations
@@ -127,24 +125,54 @@ class Parameters:
             logger.info('run END')
             sv = results.get_statevector()
             time=num_elec_it*self.delta_t
-            fname = f"{dirname}/{basename}.{self.n1}b.mu{self.x0}t{time}.csv"
+            fname = self._make_filename(outdir, time)
             logger.info("Saving to : %s", fname)
             svec.save_to_file(fname, sv, eps=1e-12)
+    
+    def draw_graph(self, outdir, times: list[int]):
+        """ draw the graph based on the results file."""
+        fig, [ax1, ax2] = plt.subplots(2, 1, figsize=(6,7))
+        ax1.set_title("amplitude")
+        ax2.set_title("phase")
+        x = np.linspace(0, self.L, self.M + 1)
+        def wrap(x):
+            return np.append(x, x[:1])
+        for tm in times:
+            fname = self._make_filename(outdir, tm)
+            logger.info("Reading: %s", fname)
+            qc = self.stm_block.circuit
+            sv = svec.read_from_file(fname)
+            data = svec.extract_dist(qc, sv, "e0x", eps=1e-12)
+            norm = np.linalg.norm(data)
+            logger.info("norm(t=%d)=%f", tm, norm)
+            y = wrap(data)
+            a = np.abs(y) / math.sqrt(self.dq)
+            th = np.angle(y)/math.pi
+            ax1.plot(x, a, label=f"t={tm}")
+            ax2.plot(x, th, label=f"t={tm}")
+        
+        ax1.legend()
+        ax2.legend()
+        fig.savefig(outdir + "/ex0_dist.png")
 
 
-def run_experiment(par: Parameters, tag):
+def run_experiment(par: Parameters, tag: str):
     outdir = "output/" + tag
     os.makedirs(outdir, exist_ok=True)
 
     logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(name)s] %(message)s',
                         filename= outdir + '/time_evo_h1.log', encoding='utf-8',
-                        level=logging.INFO,
+                        level=logging.WARNING,
                         datefmt='%Y-%m-%d %H:%M:%S')
-    logger = logging.getLogger('TEV')
     logger.setLevel(logging.INFO)
 
     par.draw_circuits(outdir)
-    par.run_circuit(outdir)
+    if par.use_saved_data:
+        logger.info('skip running the simulator')
+    else:
+        par.run_circuit(outdir)
+    times = [0]
+    par.draw_graph(outdir, times)
     logger.info('done')
 
 
@@ -153,18 +181,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="time_evo_h1",
                                      description='Time evolution of H atom')
     parser.add_argument('--device', type=str, default="CPU")
-    parser.add_argument('--cuStateVec_enable', type=str, default="False")
+    parser.add_argument('--enable-cuStateVec', type=str, default="False")
     parser.add_argument('--dim', type=int, default=1)
     parser.add_argument('--precision', type=str, default="single")
     parser.add_argument('--bits', type=int, default=5)
+    parser.add_argument('--use-saved-data', type=str, default="False")
     args = parser.parse_args()
-    print("Device : ", args.device)
-    print("cuStateVec_enable : ", args.cuStateVec_enable)
-    print("Dimension : ", args.dim)
-    print("Precision : ", args.precision)
-    use_cuStateVec = "cuStateVec" if args.cuStateVec_enable == 'True' else "statevector"
+    logger.info("Device : %s", args.device)
+    logger.info("enable_cuStateVec : %s", args.enable_cuStateVec)
+    logger.info("Dimension : %s", args.dim)
+    logger.info("Precision : %s", args.precision)
+    use_cuStateVec = "cuStateVec" if args.enable_cuStateVec == 'True' else "statevector"
     tag=f'{args.device}_{use_cuStateVec}_{args.dim}D_{args.precision}_{args.bits}b'
-    print("Tag : ", tag)
+    logger.info("Tag : %s", tag)
 
-    par = Parameters(args.device, args.cuStateVec_enable == 'True', args.dim, args.precision, args.bits)
+    par = Parameters(args.device, args.enable_cuStateVec == "True", args.dim, args.precision, args.bits, args.use_saved_data == "True")
     run_experiment(par, tag)
