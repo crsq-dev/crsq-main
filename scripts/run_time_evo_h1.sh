70e998957d097f95cf1a9f80eb5115e27768d5a3
Hideo Takahashi <takahashi-hideo543@g.ecc.u-tokyo.ac.jp>
2025-01-20 12:55:08 +0900

    added iters arg to run_time_evo_h1.sh

--- scripts/run_time_evo_h1.sh
+++ scripts/run_time_evo_h1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-if [ "$#" -ne 6 ]; then
-    echo "Usage: $0 <device> <cuStateVec_enable> <dim> <precision> <bits> <useSavedFile>"
+if [ "$#" -ne 8 ]; then
+    echo "Usage: $0 <device> <cuStateVec_enable> <dim> <precision> <bits> <useSavedFile> <nucl-iters> <elec-iters>"
     exit 1
 fi
 
@@ -9,4 +9,5 @@ source ../../.venv/bin/activate
 source ../../.env
 export PYTHONPATH
 
-python3 ./time_evo_h1.py --device $1 --enable-cuStateVec $2 --dim $3 --precision $4 --bits $5 --use-saved-data $6
+python3 ./time_evo_h1.py --device $1 --enable-cuStateVec $2 --dim $3 --precision $4 --bits $5 \
+ --use-saved-data $6 --num-nucl-iters $7 --num-elec-iters $8
