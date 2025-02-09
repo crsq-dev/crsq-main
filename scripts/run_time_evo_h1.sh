37c7412781686cb691a4bdd3c3a5fdef3b12a133
Hideo Takahashi <takahashi-hideo543@g.ecc.u-tokyo.ac.jp>
2025-02-09 09:15:30 +0900

    env settings from run script commented out

--- scripts/run_time_evo_h1.sh
+++ scripts/run_time_evo_h1.sh
@@ -5,9 +5,9 @@ if [ "$#" -ne 8 ]; then
     exit 1
 fi
 
-source ../../.venv/bin/activate
-source ../../.env
-export PYTHONPATH
+#source ../../.venv/bin/activate
+#source ../../.env
+#export PYTHONPATH
 
 python3 ./time_evo_h1.py --device $1 --enable-cuStateVec $2 --dim $3 --precision $4 --bits $5 \
  --use-saved-data $6 --num-nucl-iters $7 --num-elec-iters $8
--- scripts/run_time_evo_h1_2D.sh
+++ scripts/run_time_evo_h1_2D.sh
@@ -5,8 +5,8 @@ if [ "$#" -ne 9 ]; then
     exit 1
 fi
 
-source ../../.venv/bin/activate
-source ../../.env
-export PYTHONPATH
+#source ../../.venv/bin/activate
+#source ../../.env
+#export PYTHONPATH
 
 python3 ./time_evo_2D_h1.py --device $1 --enable-cuStateVec $2 --precision $3 --bits $4 --use-saved-data $5 --qnum-n $6 --qnum-m $7 --num-elec-iters $8 --num-nucl-iters $9
