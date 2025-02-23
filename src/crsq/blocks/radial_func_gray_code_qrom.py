16d93f5911b0c976fbf1d7d087fb09ce432f0202
Hideo Takahashi <takahashi-hideo543@g.ecc.u-tokyo.ac.jp>
2025-02-23 14:14:20 +0900

    info message removed

--- src/crsq/blocks/radial_func_gray_code_qrom.py
+++ src/crsq/blocks/radial_func_gray_code_qrom.py
@@ -199,7 +199,7 @@ class RadialFuncGrayCodeQrom1d(Frame):
             if r == 0:
                 r = self._dq/2
             psi = self._rfunc(r)
-            logger.info("i=%d, q=%f, r=%f, psi=%f", i, q, r, psi)
+            # logger.info("i=%d, q=%f, r=%f, psi=%f", i, q, r, psi)
             if abs(psi) > math.pi:
                 logger.warning("large value of |psi| at x=%f, r=%f, psi=%f", si, r, psi)
             self._data[i] = -2.0 * psi
