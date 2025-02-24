91925b397aa054f3611f54aec2a1cda7a9962b70
Hideo Takahashi <takahashi-hideo543@g.ecc.u-tokyo.ac.jp>
2025-02-24 16:47:12 +0900

    formatted.

--- src/crsq/reports/h1d1report.py
+++ src/crsq/reports/h1d1report.py
@@ -1,9 +1,8 @@
-
 from qiskit import QuantumCircuit
 import numpy as np
 import numpy.typing as npt
 import ffmpeg
-import os;
+import os
 import glob
 
 from typing import Tuple, Callable
@@ -19,9 +18,9 @@ import logging
 
 logger = logging.getLogger("crsq.reports")
 
+
 class H1D1Report:
-    """Report generator for 1 H atom, 1 dimension
-    """
+    """Report generator for 1 H atom, 1 dimension"""
 
     def __init__(
         self,
@@ -34,10 +33,10 @@ class H1D1Report:
         delta_t: float,
         total_time: float,
         num_elec_iters: int,
-        num_nucl_iters: int
+        num_nucl_iters: int,
     ):
         self._outdir = outdir
-        self._framesdir = outdir + "/frames"
+        self._frames_dir = outdir + "/frames"
         self._title = title
         self._n1 = num_coordinate_bits
         self._M = 1 << self._n1
@@ -49,28 +48,33 @@ class H1D1Report:
         self._dq = space_length / self._M
         self._delta_t = delta_t
         self._total_time = total_time
-        self._T = delta_t*num_elec_iters*num_nucl_iters
+        self._T = delta_t * num_elec_iters * num_nucl_iters
         self._num_elec_iters = num_elec_iters
         self._num_nucl_iters = num_nucl_iters
-        self._xv = np.linspace(0, self._M-1, self._M)
+        self._xv = np.linspace(0, self._M - 1, self._M)
         self._qv = self._xv * self._dq
         # potential energy function
         self._hpv = np.ndarray(self._M, np.float64)
         for x in range(self._M):
             self._hpv[x] = self._hp_func(x * self._dq)
-        self._kv = np.concatenate([np.linspace(0, self._M/2-1, self._M//2), np.linspace(-self._M/2, -1, self._M//2)])
-        self._dp = 2*math.pi/self._L
+        self._kv = np.concatenate(
+            [
+                np.linspace(0, self._M / 2 - 1, self._M // 2),
+                np.linspace(-self._M / 2, -1, self._M // 2),
+            ]
+        )
+        self._dp = 2 * math.pi / self._L
         self._pv = self._kv * self._dp
         me = 1.0
         # kinetic energy function
-        self._hkv = np.square(self._pv)/(2.0*me)
+        self._hkv = np.square(self._pv) / (2.0 * me)
         self._prepare_dir()
 
     def _prepare_dir(self) -> None:
-        dirname = self._framesdir
+        dirname = self._frames_dir
         if not os.path.exists(dirname):
             os.makedirs(dirname)
-        for f in glob.glob(f'{dirname}/t_*.png'):
+        for f in glob.glob(f"{dirname}/t_*.png"):
             os.remove(f)
         if os.path.exists(self.moviefile):
             os.remove(self.moviefile)
@@ -94,30 +98,21 @@ class H1D1Report:
         self._trace_time = []
         self._hk_trace = []
         self._hp_trace = []
-    
+
     def add_q_state_vector_file(self, t: float, svdim: int, svdata):
         """Add a q-space statevector to the report"""
-        fname = self._framesdir + "/" + f"state_vector_{t:04.3f}_q.csv"
+        fname = self._frames_dir + "/" + f"state_vector_{t:04.3f}_q.csv"
         logger.info("Saving to : %s", fname)
         svec.save_svdata_to_file(fname, svdim, svdata, eps=1e-12)
-    
+
     def add_p_state_vector_file(self, t: float, svdim: int, svdata):
         """Add a p-space statevector to the report"""
-        fname = self._framesdir + "/" + f"state_vector_{t:04.3f}_p.csv"
+        fname = self._frames_dir + "/" + f"state_vector_{t:04.3f}_p.csv"
         logger.info("Saving to : %s", fname)
         svec.save_svdata_to_file(fname, svdim, svdata, eps=1e-12)
-    
+
     def read_q_state_vector_file(self, t: float, bit_range: Tuple[int, int]):
-        fname = self._framesdir + "/" + f"state_vector_{t:04.3f}_q.csv"
-        logger.info("Reading: %s", fname)
-        sv = svec.read_from_file(fname)
-        data = svec.extract_dist_sub(sv, bit_range[0], bit_range[1], eps=1e-12)
-        norm = np.linalg.norm(data)
-        logger.info("norm(t=%d)=%f", t, norm)
-        return data
-    
-    def read_p_state_vector_file(self, t: float, bit_range: Tuple[int, int]):
-        fname = self._framesdir + "/" + f"state_vector_{t:04.3f}_p.csv"
+        fname = self._frames_dir + "/" + f"state_vector_{t:04.3f}_q.csv"
         logger.info("Reading: %s", fname)
         sv = svec.read_from_file(fname)
         data = svec.extract_dist_sub(sv, bit_range[0], bit_range[1], eps=1e-12)
@@ -125,7 +120,21 @@ class H1D1Report:
         logger.info("norm(t=%d)=%f", t, norm)
         return data
 
-    def add_wave_function_plot(self, time: float, q_data: npt.NDArray[np.complex128], p_data: npt.NDArray[np.complex128]):
+    def read_p_state_vector_file(self, t: float, bit_range: Tuple[int, int]):
+        fname = self._frames_dir + "/" + f"state_vector_{t:04.3f}_p.csv"
+        logger.info("Reading: %s", fname)
+        sv = svec.read_from_file(fname)
+        data = svec.extract_dist_sub(sv, bit_range[0], bit_range[1], eps=1e-12)
+        norm = np.linalg.norm(data)
+        logger.info("norm(t=%d)=%f", t, norm)
+        return data
+
+    def add_wave_function_plot(
+        self,
+        time: float,
+        q_data: npt.NDArray[np.complex128],
+        p_data: npt.NDArray[np.complex128],
+    ):
         """Add a statevector to the report"""
         ab = np.abs(q_data) / math.sqrt(self._dq)
         re = np.real(q_data) / math.sqrt(self._dq)
@@ -135,14 +144,10 @@ class H1D1Report:
         self._axs[2].plot(self._qv, im, label=f"t={time:4.3f}")
         """ add a frame for the video """
         self._produce_video_frame(time, q_data, p_data)
-    
+
     def record_energy(self, t, q_data, p_data):
-        Hk = np.sum(
-            np.abs(p_data * np.conjugate(p_data)) * self._hkv
-        ).item()
-        Hp = np.sum(
-            np.abs(q_data * np.conjugate(q_data)) * self._hpv
-        ).item()
+        Hk = np.sum(np.abs(p_data * np.conjugate(p_data)) * self._hkv).item()
+        Hp = np.sum(np.abs(q_data * np.conjugate(q_data)) * self._hpv).item()
         self._trace_time.append(t)
         self._hk_trace.append(Hk)
         self._hp_trace.append(Hp)
@@ -167,31 +172,35 @@ class H1D1Report:
         plt.close(fig)
 
     def _produce_video_frame(self, t: float, q_data, p_data):
-        fig, axs = plt.subplots(3, 1, figsize=(8, 12), layout='constrained')
+        fig, axs = plt.subplots(3, 1, figsize=(8, 12), layout="constrained")
         fig.suptitle(self._title + f" t={t:6.3f}")
         self._produce_psiq_frame(t, axs[0], q_data)
         self._produce_psip_frame(t, axs[1], p_data)
         self._produce_logpsip_frame(t, axs[2], p_data)
-        filename = f'{self._framesdir}/t_{t:06.3f}.png'
+        filename = f"{self._frames_dir}/t_{t:06.3f}.png"
         print("writing to file : ", filename)
         fig.savefig(filename)
         plt.close(fig)
 
-    def _produce_psiq_frame(self, t: float, ax: plt.Axes, q_data: npt.NDArray[np.complex128]):
+    def _produce_psiq_frame(
+        self, t: float, ax: plt.Axes, q_data: npt.NDArray[np.complex128]
+    ):
         ax.set_ylim([-1, 1])
         rdq = math.sqrt(self._dq)
         np_qv = self._qv
         np_psi5q = q_data
         psi_label = "ψ_q"
         ax.set_title(psi_label)
-        ax.plot(np_qv, (1/rdq)*np.abs(np_psi5q), label='|ψ(q)|')
-        ax.plot(np_qv, (1/rdq)*np.real(np_psi5q), label='Re(ψ(q))')
-        ax.plot(np_qv, (1/rdq)*np.imag(np_psi5q), label='Im(ψ(q))')
-        ax.set_xlabel('q')
-        ax.set_ylabel('amplitude')
+        ax.plot(np_qv, (1 / rdq) * np.abs(np_psi5q), label="|ψ(q)|")
+        ax.plot(np_qv, (1 / rdq) * np.real(np_psi5q), label="Re(ψ(q))")
+        ax.plot(np_qv, (1 / rdq) * np.imag(np_psi5q), label="Im(ψ(q))")
+        ax.set_xlabel("q")
+        ax.set_ylabel("amplitude")
         ax.legend()
 
-    def _produce_psip_frame(self, t: float, ax: plt.Axes, p_data: npt.NDArray[np.complex128]):
+    def _produce_psip_frame(
+        self, t: float, ax: plt.Axes, p_data: npt.NDArray[np.complex128]
+    ):
         # 離散波数が -WM ~ WM の範囲の成分を表示する
         ax.set_ylim([-1.0, 1.0])
 
@@ -199,39 +208,41 @@ class H1D1Report:
         WM = self._WM
 
         pv1 = self._pv[0:WM]
-        pv2 = self._pv[M-WM:M]
+        pv2 = self._pv[M - WM : M]
         np_pv = np.concatenate([pv2, pv1])
 
         ps4p1 = p_data[0:WM]
-        ps4p2 = p_data[M-WM:M]
+        ps4p2 = p_data[M - WM : M]
         np_psi4p = np.concatenate([ps4p2, ps4p1])
 
         rdp = math.sqrt(self._dp)
         psi_label = "ψ_p"
         ax.set_title(psi_label)
-        ax.plot(np_pv, (1/rdp)*np.abs(np_psi4p), label='|ψ\u0303(p)|')
-        ax.plot(np_pv, (1/rdp)*np.real(np_psi4p), label='Re(ψ\u0303(p))')
-        ax.plot(np_pv, (1/rdp)*np.imag(np_psi4p), label='Im(ψ\u0303(p))')
+        ax.plot(np_pv, (1 / rdp) * np.abs(np_psi4p), label="|ψ\u0303(p)|")
+        ax.plot(np_pv, (1 / rdp) * np.real(np_psi4p), label="Re(ψ\u0303(p))")
+        ax.plot(np_pv, (1 / rdp) * np.imag(np_psi4p), label="Im(ψ\u0303(p))")
         # the xlabel will clash with the title of the third graph.
         # ax.set_xlabel('p')
-        ax.set_ylabel('amplitude')
+        ax.set_ylabel("amplitude")
         ax.legend()
 
-    def _produce_logpsip_frame(self, t: float, ax: plt.Axes, p_data: npt.NDArray[np.complex128]):
+    def _produce_logpsip_frame(
+        self, t: float, ax: plt.Axes, p_data: npt.NDArray[np.complex128]
+    ):
         ax.set_ylim([-30, 0])  # log range -30 to 0
         ax.grid(True)
         M = self._M
         WM = self._WM
 
         pv1 = self._pv[0:WM]
-        pv2 = self._pv[M-WM:M]
+        pv2 = self._pv[M - WM : M]
         np_pv = np.concatenate([pv2, pv1])
 
         logpsi = np.log2(np.abs(p_data))
         logp1 = logpsi[0:WM]
-        logp2 = logpsi[M-WM:M]
+        logp2 = logpsi[M - WM : M]
         np_logp = np.concatenate([logp2, logp1])
-        ax.plot(np_pv, np_logp, label='log2|ψ\u0303(p)|\u221adp')
+        ax.plot(np_pv, np_logp, label="log2|ψ\u0303(p)|\u221adp")
 
         # logt = np.log2(self._tarray)
         # logt1 = logt[0:WM]
@@ -239,16 +250,16 @@ class H1D1Report:
         # np_logt = np.asnumpy(np.concatenate([logt2, logt1]))
         # ax.plot(np_pv, np_logt, label='log2(T)')
 
-        ax.set_title(f'log2|ψ\u0303(p)|\u221adp')
+        ax.set_title(f"log2|ψ\u0303(p)|\u221adp")
 
-        ax.set_xlabel('p [rad/bohr]')
-        ax.set_ylabel('log2(|ψ|)')
+        ax.set_xlabel("p [rad/bohr]")
+        ax.set_ylabel("log2(|ψ|)")
         ax.legend()
 
     @property
     def tagname(self):
         return f"ex0_{self._n1}b.{self._num_nucl_iters}n.{self._num_elec_iters}e"
-    
+
     @property
     def moviefile(self):
         return f"{self._outdir}/{self.tagname}.mp4"
@@ -257,9 +268,7 @@ class H1D1Report:
         self._axs[0].legend()
         # axs[1].legend()
         # axs[2].legend()
-        self._fig.savefig(
-            self._outdir + "/" + self.tagname + ".dist.png"
-        )
+        self._fig.savefig(self._outdir + "/" + self.tagname + ".dist.png")
         plt.close(self._fig)
 
         self._plot_energy()
@@ -267,5 +276,7 @@ class H1D1Report:
 
     def _produce_video(self):
         print("producing video : ", self.moviefile)
-        stream = ffmpeg.input(f'{self._framesdir}/t_*.png', pattern_type='glob', framerate=8)
-        ffmpeg.output(stream, self.moviefile, pix_fmt='yuv420p').run()
+        stream = ffmpeg.input(
+            f"{self._frames_dir}/t_*.png", pattern_type="glob", framerate=8
+        )
+        ffmpeg.output(stream, self.moviefile, pix_fmt="yuv420p").run()
